3743678a8ed33ae3f3b9868bb4d5e881c5428463
Dikshyanta Uprety <78378956+Dikshyanta23@users.noreply.github.com>
2023-10-10 21:15:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(notFoundMiddleware)
 //security packages
 app.set('trust proxy', 1);
 app.use(
-  rateLimit({
+  rateLimiter({
     windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
     standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
